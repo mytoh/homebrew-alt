effb24aaae8e33ba204aa9419fe3c23d50d81c09
mytoh <mytoh@gmail.com>
2012-10-13 16:45:59 +0900

    update cask

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -1,4 +1,4 @@
-class CooViewer < Cask
+class Cooviewer < Cask
   homepage 'http://www.geocities.jp/coo_ona/viewer.html'
   version  '1.2b'
   url      'http://www.geocities.jp/coo_ona/cooviewer1.2b24.dmg'
